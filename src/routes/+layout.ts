4065d1af031e13ecc4bb7f44a9a2cd5e87fe8b75
lafkpages <soy.lafk+gh@gmail.com>
2023-07-01 21:22:42 +0000

    Move prerender entries to a server page file

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,2 @@
-import type { EntryGenerator } from './$types';
-
 export const prerender = true;
 export const trailingSlash = 'always';
-
-export const entries: EntryGenerator = () => [
-  '/',
-  '/fileHandler'
-];
