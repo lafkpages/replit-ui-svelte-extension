dec325fcf0db84fd9bdad1346e83ff88a4de1d2e
lafkpages <soy.lafk+gh@gmail.com>
2023-07-01 19:18:25 +0000

    Show file handler file in file handler route
    Note that this is currently broken due to an error in the Replit Extensions lib.

--- src/routes/fileHandler/+page.svelte
+++ src/routes/fileHandler/+page.svelte
@@ -1,1 +1,20 @@
-This is a file handler. You selected the file <code>TODO</code>
+<script lang="ts">
+  // Replit Extensions API
+  import { me } from '@replit/extensions';
+</script>
+
+This is a file handler. You selected the file:
+
+{#await me?.filePath()}
+  <i>Loading file</i>
+{:then file}
+  <code>{file}</code>
+{:catch error}
+  <i>
+    Unexpected error:
+    
+    <code>
+      {error}
+    </code>
+  </i>
+{/await}
