17a44153bff630624ed48c551b2db1eb80c11c53
LuisAFK <soy.lafk+gh@gmail.com>
2023-07-02 15:33:18 +0000

    Comment in on boot script

--- scripts/onBoot.sh
+++ scripts/onBoot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+# Symlink scripts into .config/bin, which should be in PATH
 cd scripts
-
 for file in *; do
   dest=`realpath "../.config/bin/${file:0:-3}"`
   if [ ! -f "$dest" ]; then
