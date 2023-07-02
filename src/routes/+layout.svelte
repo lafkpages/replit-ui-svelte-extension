94c3fb72af61775ef08a2955d061f2f11b44302d
lafkpages <soy.lafk+gh@gmail.com>
2023-07-02 13:46:09 +0000

    Get extension file handler path onMount, helps detect handshake errir

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,13 @@
   import ToastProvider from '@replit-svelte/ui/ToastProvider.svelte';
   import Loader from '@replit-svelte/ui/icons/Loader.svelte';
 
-  import { init, HandshakeStatus } from '@replit/extensions';
+  import { init, me, HandshakeStatus } from '@replit/extensions';
   import type { ReplitInitOutput } from '@replit/extensions';
 
   import { onMount, onDestroy } from 'svelte';
 
   let handshakeResult: ReplitInitOutput | null = null;
+  let filePath: string | null | undefined = null;
 
   onMount(() => {
     init()
@@ -17,6 +18,10 @@
         handshakeResult = res;
 
         console.debug(res);
+
+        me.filePath().then(file => {
+          filePath = file;
+        }).catch(console.error);
       })
       .catch(err => {
         console.error('Error init()ing the Replit Extensions API:', err);
@@ -31,13 +36,13 @@
 <ModalProvider>
   <ToastProvider>
     <main>
-      {#if handshakeResult?.status == HandshakeStatus.Ready}
-        <slot />
-      {:else if handshakeResult?.status == HandshakeStatus.Error}
+      {#if handshakeResult?.status == HandshakeStatus.Error || filePath === undefined}
         <h1 class="headerBig">Handshake Error</h1>
         <p>
           We couldn't connect to the Replit workspace. Make sure you're running this as an extension.
         </p>
+      {:else if handshakeResult?.status == HandshakeStatus.Ready}
+        <slot />
       {:else}
         <div class="handshakeLoading">
           <Loader />
