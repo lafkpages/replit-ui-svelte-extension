37b2207e6c0bcb78d8b692684c3c3d075f23b228
lafkpages <soy.lafk+gh@gmail.com>
2023-09-11 14:57:12 +0000

    Remove the extension ID from .replit config when forked

--- scripts/onFork.sh
+++ scripts/onFork.sh
@@ -9,3 +9,6 @@ echo "1" > /tmp/_replit_ui_svelte_extension_forked
 
 # Remove template's Git repository
 rm -rf ".git"
+
+# Remove the extension ID from .replit config
+sed -ri 's/^extensionID\s*=.+$//m' .replit
