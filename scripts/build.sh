31ec11ab60630e2bfcbec69cd951bd3b2d03adf4
lafkpages <soy.lafk+gh@gmail.com>
2023-09-11 18:25:27 +0000

    Better build script
    https://github.com/lafkpages/replit-extension-dotenv/commits/main/scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # Disable LSPs to have more RAM
-disableSvelteServer
+"$REPL_HOME/scripts/disableSvelteServer.sh"
 
 # Run the SvelteKit build adapter
+set -o pipefail
 pnpm run build
+set +o pipefail
 
 # Reenable LSPs
-enableSvelteServer
+"$REPL_HOME/scripts/enableSvelteServer.sh"
