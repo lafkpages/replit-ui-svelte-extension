b607dcaa7e5dd3e3233e581b0c1b1523fa7d7aca
lafkpages <soy.lafk+gh@gmail.com>
2023-09-11 16:03:00 +0000

    onFork script pipefail

--- scripts/onFork.sh
+++ scripts/onFork.sh
@@ -3,6 +3,10 @@
 # This only runs when the Repl boots after being forked.
 # This will run once, and then self-destruct
 
+# If any of the bellow commands fail,
+# the script will exit with a non-zero exit code
+set -o pipefail
+
 
 # Debug logs
 echo "1" > /tmp/_replit_ui_svelte_extension_forked
